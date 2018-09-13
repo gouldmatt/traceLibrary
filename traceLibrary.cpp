ec7c408e283a4cd1acc5363ecbe2dbe2493bf016
Matthew Gould <mattgould44@gmail.com>
2018-09-12 22:03:21 -0700

    > <  <>  ><

--- traceLibrary.cpp
+++ traceLibrary.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <chrono> 
 #include "traceLibrary.hpp"
 
 using namespace std; 
 
-void traceLibrary::trace_start(char* filename){
-
-    cout << "hello world";
+void traceLibrary::trace_start(char* filename){ 
+    currentEvent = 0; 
 }
-
 
 void traceLibrary::trace_end(){
 
 }
 
+void traceLibrary::trace_event_start(char* name){
+    //start time measurement
+    //eventsArr[currentEvent].startTime = chrono::system_clock::now();
+
+}
+
+void traceLibrary::trace_event_end(){
+    //end time measurement
+    auto end = chrono::steady_clock::now();
+
+}
+
 int main(){
-    
+    return 0;
 }
--- traceLibrary.hpp
+++ traceLibrary.hpp
@@ -2,13 +2,25 @@
 #ifndef TRACELIBRARY_HPP
 #define TRACELIBRARY_HPP
 
+struct traceEvent{
+    double startTime;
+    double endTime;  
+    char nameBuffer[100];
+};
+
 class traceLibrary{
 public: 
     void trace_start(char* filename);
 
     void trace_end(); 
 
+    void trace_event_start(char* name);
+
+    void trace_event_end();
+
 private: 
+    int currentEvent;
+    traceEvent eventsArr[10000]; 
 };
 
 #endif 
